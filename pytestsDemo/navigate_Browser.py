c075d27eef25dea9e295e5f74a9c92d3320cd176
Akhilesh68 <69416533+Akhilesh68@users.noreply.github.com>
2025-12-28 21:20:03 +0530

    Update navigate_Browser.py

--- pytestsDemo/navigate_Browser.py
+++ pytestsDemo/navigate_Browser.py
@@ -6,6 +6,7 @@ driver=webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://www.tutorialspoint.com/jenkins/jenkins_setup_build_jobs.htm")
 driver.close()
+print("Hello Python")
 
 
 # C:\Users\Akhilesh\PycharmProjects\PythonTesting1\pytestsDemo
